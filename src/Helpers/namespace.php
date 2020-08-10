1096fcf2afdf93c3e93fcdc326ab9c68c0bd02e7
Scott Carpenter <scott@payforstay.com>
2020-08-10 15:11:27 -0600

    Remove `app_namespace()` helper

--- src/Helpers/namespace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('app_namespace')) {
-    function app_namespace()
-    {
-        return app('Grafite\CrudMaker\Services\AppService')
-            ->getAppNamespace();
-    }
-}
